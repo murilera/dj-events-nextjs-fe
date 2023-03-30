f52ccdb54ef9ca60da45cc7d28e506d88b1c3b18
Murilo Rodegheri <murilo.rodegheri@dowjones.com>
2023-03-30 00:43:29 -0300

    feat: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Layout from '@/components/Layout'
+import Link from 'next/link'
+import styles from '../styles/404.module.css'
+
+const NotFoundPage = () => {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
+        <h4>Sorry, nothing here</h4>
+        <Link href='/'> Go back home </Link>
+      </div>
+
+    </Layout>
+  )
+}
+
+export default NotFoundPage
