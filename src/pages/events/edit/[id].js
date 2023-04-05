b196bca50e96493371676d697ed987d44a278615
Murilo Rodegheri <murilo.rodegheri@dowjones.com>
2023-04-04 23:32:02 -0300

    feat: add edit event feature

---
+++ src/pages/events/edit/[id].js
@@ -0,0 +1,166 @@
+import Layout from "@/components/Layout"
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { API_URL } from "@/config"
+import styles from '@/styles/Form.module.css'
+import Link from "next/link"
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import moment from "moment"
+
+const EditEventPage = ({ evt: { id, attributes } }) => {
+
+  const [values, setValues] = useState({
+    name: attributes.name,
+    performers: attributes.performers,
+    venue: attributes.venue,
+    address: attributes.address,
+    date: attributes.date,
+    time: attributes.time,
+    description: attributes.description
+  })
+
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    // Validation
+    const hasEmptyFields = Object.values(values).some(
+      (element) => element === ''
+    )
+
+    if (hasEmptyFields) {
+      toast.error('Please fill in all fields')
+    } else {
+      const data = {
+        data: values
+      }
+
+      console.log(data)
+
+      const res = await fetch(`${API_URL}/api/events/${id}`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+
+      if (!res.ok) {
+        if (res.status === 403 || res.status === 401) {
+          toast.error('No token included')
+          return
+        }
+        toast.error('Something Went Wrong')
+      } else {
+        const evt = await res.json()
+        router.push(`/events/${evt.data.attributes.slug}`)
+      }
+    }
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
+
+  return (
+    <Layout title='Edit Event'>
+      <Link href='events'>Go Back</Link>
+      <h1>Edit Event</h1>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor='name'>Event Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='performers'>Performers</label>
+            <input
+              type='text'
+              name='performers'
+              id='performers'
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='venue'>Venue</label>
+            <input
+              type='text'
+              name='venue'
+              id='venue'
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='address'>Address</label>
+            <input
+              type='text'
+              name='address'
+              id='address'
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='date'>Date</label>
+            <input
+              type='date'
+              name='date'
+              id='date'
+              value={moment(values.date).format('yyyy-MM-DD')}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='time'>Time</label>
+            <input
+              type='text'
+              name='time'
+              id='time'
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor='description'>Event Description</label>
+          <textarea
+            type='text'
+            name='description'
+            id='description'
+            value={values.description}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+
+        <input type="submit" value="Update Event" className="btn" />
+      </form>
+    </Layout>
+  )
+}
+
+export default EditEventPage
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await fetch(`${API_URL}/api/events/${id}`)
+  const evt = await res.json()
+
+  return {
+    props: {
+      evt: evt.data
+    }
+  }
+}
